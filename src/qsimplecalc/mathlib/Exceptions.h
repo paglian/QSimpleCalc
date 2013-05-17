a9d75ed2f12450ba9f035a53b9b39579ca856adf
Andres Pagliano <andres.pagliano@lvklabs.com>
2013-05-17 12:02:34 -0300

    Added Exceptions.h

---
+++ src/qsimplecalc/mathlib/Exceptions.h
@@ -0,0 +1,49 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+#include <QString>
+#include <exception>
+
+/**
+ * @brief The MathException class is the base class for all math exceptions
+ */
+class MathException : public std::exception
+{
+};
+
+/**
+ * @brief The InvalidSyntaxException class
+ */
+class InvalidSyntaxException : public MathException
+{
+};
+
+/**
+ * @brief The DivByZeroException class
+ */
+class DivByZeroException : public MathException
+{
+};
+
+/**
+ * @brief The LogZeroException class
+ */
+class LogZeroException : public MathException
+{
+};
+
+/**
+ * @brief The LogNegativeException class
+ */
+class LogNegativeException : public MathException
+{
+};
+
+/**
+ * @brief The NullValueException class
+ */
+class NullValueException : public MathException
+{
+};
+
+#endif // EXCEPTIONS_H

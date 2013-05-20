68581b722c7c100ffb0b3829578fb338fd0cb471
Andres Pagliano <andres.pagliano@lvklabs.com>
2013-05-20 09:47:30 -0300

    Documented string constants

--- src/qsimplecalc/mathlib/StringConstants.h
+++ src/qsimplecalc/mathlib/StringConstants.h
@@ -1,15 +1,15 @@
 #ifndef STRINGCONSTANTS_H
 #define STRINGCONSTANTS_H
 
-#define STR_ADD     "+"
-#define STR_SUBS    "-"
-#define STR_MULT    "*"
-#define STR_DIV     "/"
-#define STR_LOG     "log "
-#define STR_OPAR    "("
-#define STR_CPAR    ")"
-#define STR_DOT     "."     // Decimal mark
-#define STR_X       "x"     // Variable in equations
-#define STR_EQ      "="     // Equality in equations
+#define STR_ADD     "+"     /// Addition
+#define STR_SUBS    "-"     /// Substraction
+#define STR_MULT    "*"     /// Multiplication
+#define STR_DIV     "/"     /// Division
+#define STR_LOG     "log "  /// Logarithm base 10
+#define STR_OPAR    "("     /// Open parentheses
+#define STR_CPAR    ")"     /// Close parentheses
+#define STR_DOT     "."     /// Decimal mark
+#define STR_X       "x"     /// Variable in equations
+#define STR_EQ      "="     /// Equality in equations
 
 #endif // STRINGCONSTANTS_H
